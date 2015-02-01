839aab9f4d0ae94241b068f0a8e27bcebd105483
Alasdair Smith <alasdairsmith100@gmail.com>
2015-02-01 23:55:39 +0000

    Removing more unnecessary comments

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -7,17 +7,6 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reset Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller is responsible for handling password reset requests
-	| and uses a simple trait to include this behavior. You're free to
-	| explore this trait and override any methods you wish to tweak.
-	|
-	*/
-
 	use ResetsPasswords;
 
 	/**
@@ -25,7 +14,6 @@ class PasswordController extends Controller {
 	 *
 	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
 	 * @param  \Illuminate\Contracts\Auth\PasswordBroker  $passwords
-	 * @return void
 	 */
 	public function __construct(Guard $auth, PasswordBroker $passwords)
 	{
