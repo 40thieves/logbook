a9e0c0bee55693b516d9f7eacf737d7ec59e8970
Alasdair Smith <alasdairsmith100@gmail.com>
2015-02-08 14:35:10 +0000

    Remove unnecessary slashes from routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,11 +2,12 @@
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/login', 'Auth\AuthController@getLogin');
-Route::post('/login', 'Auth\AuthController@postLogin');
-Route::get('/logout', 'Auth\AuthController@getLogout');
+Route::controller('auth', 'Auth\AuthController');
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
 
-Route::get('/forgot', 'Auth\PasswordController@getEmail');
-Route::post('/forgot', 'Auth\PasswordController@postEmail');
-Route::get('/reset', 'Auth\PasswordController@getReset');
-Route::post('/reset', 'Auth\PasswordController@postReset');
+Route::get('forgot', 'Auth\PasswordController@getEmail');
+Route::post('forgot', 'Auth\PasswordController@postEmail');
+Route::get('reset', 'Auth\PasswordController@getReset');
+Route::post('reset', 'Auth\PasswordController@postReset');
