e83573ad72e23e24597605b2d193326cd8fa8f8f
Alasdair Smith <alasdairsmith100@gmail.com>
2015-02-08 15:13:31 +0000

    Move controller routes down, so short routes take prescidence

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,11 +2,6 @@
 
 Route::get('/', 'HomeController@index');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
-
 Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'Auth\AuthController@getLogout');
@@ -15,3 +10,8 @@ Route::get('forgot', 'Auth\PasswordController@getEmail');
 Route::post('forgot', 'Auth\PasswordController@postEmail');
 Route::get('reset', 'Auth\PasswordController@getReset');
 Route::post('reset', 'Auth\PasswordController@postReset');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
