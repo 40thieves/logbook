88e8d549b3bdb2b0a9d6fe5a684b48e95954b396
Alasdair Smith <alasdairsmith100@gmail.com>
2015-03-07 22:39:54 +0000

    Create log command

---
+++ app/Commands/CreateLog.php
@@ -0,0 +1,50 @@
+<?php namespace App\Commands;
+
+use DateTime;
+
+class CreateLog extends Command {
+
+	/**
+	 * @var DateTime
+	 */
+	private $date;
+
+	/**
+	 * @var string
+	 */
+	private $region;
+
+	/**
+	 * @var string
+	 */
+	private $members;
+
+	/**
+	 * @var string
+	 */
+	private $description;
+
+	/**
+	 * @var boolean
+	 */
+	private $wasLeader;
+
+	/**
+	 * @var int
+	 */
+	private $status;
+
+	/**
+	 * Create a new command instance.
+	 */
+	public function __construct($date, $region, $members, $description, $wasLeader, $status)
+	{
+		$this->date = $date;
+		$this->region = $region;
+		$this->members = $members;
+		$this->description = $description;
+		$this->wasLeader = $wasLeader;
+		$this->status = $status;
+	}
+
+}
