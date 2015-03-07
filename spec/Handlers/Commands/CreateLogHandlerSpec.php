b06c6bc21d169dbde35f332c12b603aabc8f5cf8
Alasdair Smith <alasdairsmith100@gmail.com>
2015-03-07 23:37:00 +0000

    Start on create log handler spec

---
+++ spec/Handlers/Commands/CreateLogHandlerSpec.php
@@ -0,0 +1,13 @@
+<?php namespace spec\App\Handlers\Commands;
+
+use App\Domain\Logs\LogRepository;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class CreateLogHandlerSpec extends ObjectBehavior
+{
+    function it_creates_a_log(LogRepository $repo)
+    {
+        // 
+    }
+}
