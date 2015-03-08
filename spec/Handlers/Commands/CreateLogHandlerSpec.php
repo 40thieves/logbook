a3065e1b85c40ed235efbc6cea980c9920838ef0
Alasdair Smith <alasdairsmith100@gmail.com>
2015-03-08 00:07:15 +0000

    Implements test on create log handler spec

--- spec/Handlers/Commands/CreateLogHandlerSpec.php
+++ spec/Handlers/Commands/CreateLogHandlerSpec.php
@@ -1,6 +1,8 @@
 <?php namespace spec\App\Handlers\Commands;
 
+use App\Commands\CreateLog;
 use App\Domain\Logs\LogRepository;
+use DateTime;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -8,6 +10,15 @@ class CreateLogHandlerSpec extends ObjectBehavior
 {
     function it_creates_a_log(LogRepository $repo)
     {
-        // 
+        $repo->commit()->shouldBeCalled();
+
+        $this->handle(new CreateLog(
+            new DateTime(),
+            'Snowdonia',
+            'Me',
+            'Description of some walking',
+            true,
+            1
+        ));
     }
 }
